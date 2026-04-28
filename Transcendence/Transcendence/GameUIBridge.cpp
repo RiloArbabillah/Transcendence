9142c4d25b478afd22461a3015fe9123e1694faf
RiloArbabillah <rilo.arbabillah@gmail.com>
2026-04-29 01:48:02 +0800

    feat: add session source files to build
    - Add CLoadingSession, CHelpSession, CStatsSession, CLoadGameSession, CLoginSession, CNewGameSession, CChooseAdventureSession, CGameSession, CGalacticMapSession, CKeyboardMapSession, CProfileSession, CTextCrawlSession, CAdventureIntroSession, CChangePasswordSession, CModExchangeSession, CWaitSession, CLoadGameWithSignInTask
    - Add GameSessionAnimate, GameSessionCommands, GameSessionInput, GameSessionMenus
    - Simplify GameUIBridge (remove controller init attempts)

--- Transcendence/Transcendence/GameUIBridge.cpp
+++ Transcendence/Transcendence/GameUIBridge.cpp
@@ -1,39 +1,22 @@
 //	GameUIBridge.cpp
-//	Full game UI integration - shows intro menu
-//	Minimal path to get game UI displaying
+//	Game UI integration
 
 #include "Alchemy.h"
 #include "TSUI.h"
 #include "Transcendence.h"
 #include "Platform/AppCore.h"
 
+int g_cxScreen = 1024;
+int g_cyScreen = 768;
+
 void InitGameUI(SAppState& state)
 {
     CHumanInterface::Create();
-
     g_pHI->GetScreenMgr().Init(state.cxWidth, state.cyHeight, nullptr);
 }
 
 void UpdateGameUI(SAppState& state)
 {
-    if (g_pHI && g_pHI->GetSession())
-    {
+    if (g_pHI)
         g_pHI->OnAnimate();
-    }
-    else
-    {
-        static int frame = 0;
-        for (int y = 0; y < state.cyHeight; y++)
-        {
-            for (int x = 0; x < state.cxWidth; x++)
-            {
-                int idx = y * state.cxWidth + x;
-                uint8_t r = (x * 255) / state.cxWidth;
-                uint8_t g = (y * 255) / state.cyHeight;
-                uint8_t b = 128;
-                state.pFrameBuffer[idx] = (r << 16) | (g << 8) | b | 0xFF000000;
-            }
-        }
-        frame++;
-    }
 }
