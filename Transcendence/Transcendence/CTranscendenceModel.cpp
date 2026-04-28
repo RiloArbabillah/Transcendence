717116f03e434eb68960d4cc49d9418ee8cb799e
RiloArbabillah <rilo.arbabillah@gmail.com>
2026-04-28 12:46:18 +0800

    feat: add CTranscendenceModel and CodeChainExtensions
    Fix pointer cast in CTranscendenceModel

--- Transcendence/Transcendence/CTranscendenceModel.cpp
+++ Transcendence/Transcendence/CTranscendenceModel.cpp
@@ -523,7 +523,7 @@ ALERROR CTranscendenceModel::EndGame (void)
 	//	are generated properly. (It gets cleared out in CTranscendenceWnd::PlayerEnteredGate)
 
 	CCodeChain &CC = m_Universe.GetCC();
-	CC.DefineGlobal(CONSTLIT("gPlayerShip"), CC.CreateInteger((int)m_pPlayer->GetShip()));
+	CC.DefineGlobal(CONSTLIT("gPlayerShip"), CC.CreateInteger((intptr_t)m_pPlayer->GetShip()));
 
 	//	Generate stats and save to file
 
